82ec8ac51ceec59c9041bfcf9b10ace14a48ad7e
0nyX0n <jeremy.supplis@gmail.com>
2020-06-09 08:21:55 +0200

    Initial Commit

---
+++ Loader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loader
+//
+//  Created by Jérémy Supplis on 09/06/2020.
+//  Copyright © 2020 Jérémy Supplis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
